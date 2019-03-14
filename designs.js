d0bd2acfbdb565efcf1e4cec16117f48e03f4bb0
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-03-14 15:31:26 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -4,10 +4,10 @@ function makeGrid() {
   var width = document.getElementById('inputWidth').value;
   canvas.innerHTML = ''; // remove children
   for (tr = 0; tr < height; tr++) { // loop over rows/height
-    var row = document.createElement("tr"); // create row
+    var row = document.createElement('tr'); // create row
     canvas.appendChild(row); // insert row (attribution - https://knowledge.udacity.com/questions/29796)
     for (td = 0; td < width; td++) { // loop over cells/width
-      var cell = document.createElement("td"); // create cell
+      var cell = document.createElement('td'); // create cell
       row.appendChild(cell); // insert cell (attribution - https://knowledge.udacity.com/questions/29796)
       cell.addEventListener('click', function(event) { // get color on click
         var color = document.getElementById('colorPicker');
